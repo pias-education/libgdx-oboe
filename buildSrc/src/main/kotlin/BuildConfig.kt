42d144bfd7830cb785e81218abf62bf38f247b8c
Maximilian Wende <dasisdormax@mailbox.org>
2023-10-24 15:01:53 +0200

    Libraries rebuilt after build config change

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,6 +1,6 @@
 object AndroidConfig {
     const val BUILD_TOOLS = "31.0.0"
-    const val MIN_SDK = 21
+    const val MIN_SDK = 19
     const val SDK = 33
     const val NDK = "25.1.8937393"
 }
@@ -9,10 +9,10 @@ object AppConfig {
     const val PUBLISHED_GROUP_ID = "com.pias-education"
     const val ARTIFACT_NAME = "libgdx-oboe"
     const val NAME = "libGDX Oboe"
-    const val DESCRIPTION = "libGDX audio replacement for Android (API 21+) built on top of Oboe."
+    const val DESCRIPTION = "libGDX audio replacement for Android (API 19+) built on top of Oboe."
     const val SITE_URL = "https://github.com/pias-education/libgdx-oboe"
     const val GIT_URL = "git@github.com:pias-education/libgdx-oboe.git"
-    const val VERSION = "0.3.0"
+    const val VERSION = "0.3.1"
     const val DEVELOPER_ID = "pias-education"
     const val DEVELOPER_EMAIL = "admin@pias-education.com"
     const val LICENSE_NAME = "The MIT License"
