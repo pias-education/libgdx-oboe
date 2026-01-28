e48b8793fbc7f82eba7cc5b8d5fd703467a1ca13
aup <aud.peters@pias-education.com>
2026-01-28 15:52:24 +0100

    remove jniLibs/x86_64

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 
         ndk {
             //abiFilters.addAll(listOf("x86", "x86_64", "armeabi-v7a", "arm64-v8a"))
-            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86_64" ))
+            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a"))
 
             // moved into correct block
 
